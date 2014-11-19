55ddf20c89a524d449e18c78f00de7e891981843
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-11-19 09:51:51 +0100

    sync

--- Cubes7.hs
+++ Cubes7.hs
@@ -11,6 +11,7 @@ type Cube a = Colors -> a
 
 data Term = TU | TPi String Term Term | TLam String Term | TApp Term Term | TVar String | TParam Color Term | TPair Color Term Term
           | TPsi Color Term {- Domain -} Term {- Predicate -} -- Ψ i (A:U) (P:A->U)
+          | TInParam String Term Term
   deriving Show
 
 class Nominal a where
@@ -35,10 +36,25 @@ instance Nominal Term where
       TVar x -> TVar x
       TParam k t -> TParam (sw k) (sw t)
       TPair k a p -> TPair (sw k) (sw a) (sw p)
+      TPsi k a p -> TPsi (sw k) (sw a) (sw p)
 
 type Env = [(String,Cube Val)]
 
 data Dim = Incl Colors | Excl Colors Colors
+
+strictSublists = init . sublists
+
+sublists [] = [[]]
+sublists (x:xs) = sublists xs ++ map (x:) (sublists xs)
+
+cart :: [[a]] -> [[a]] -> [[a]]
+cart xs ys = do
+  x <- xs
+  y <- ys
+  return (x++y)
+dimLists :: Dim -> [[Color]]
+dimLists (Incl xs) = sublists xs
+dimLists (Excl is xs) = cart (strictSublists is) (sublists xs)
 
 incl is fresh = Incl (is ++ fresh)
 
@@ -46,60 +62,61 @@ data Val = Var String | Pi Dim (Cube Val) Val | App Dim Val (Cube Val) | Lam Dim
 
 interior :: Color -> Cube x -> Cube x
 interior i q is = q (i:is) -- FIXME: this is not correctly inserted (sort?)
+  -- Invariant: i ∉ is
 
 face :: Color -> Cube x -> Cube x
 face i q is = q (is \\ [i])
 
-appCub :: Cube Val -> Cube Val -> Cube Val
-appCub f u is = app (Incl is) (f is) u
-
-app χ (Lam _ f) x = f x
+app χ (Lam χ' f) x = f x -- invariant χ=χ' (or at least dimLists χ ⊆ dimLists χ')
 app χ n x = App χ n x
+
+appCub :: Cube Val -> Cube Val -> Cube Val
+appCub f u is = app (Incl is) (f is) u  -- implicit injection on u
 
 splitSupply [] = ([],[])
 splitSupply [x] = ([x],[])
 splitSupply (x1:x2:xs) = (x1:xs1,x2:xs2)
   where (xs1,xs2) = splitSupply xs
 
+π :: Dim -> Cube Val -> (Cube Val -> Val) -> Val
 π dim typ f = Pi dim typ $ Lam dim f
 
 eval :: Colors -> Env -> Term -> Cube Val
-eval fresh env t0 is =
-  let (fresh1,fresh2) = splitSupply fresh
-      (fresh0:freshs) = fresh
+eval φ ρ t0 χ =
+  let (φ1,φ2) = splitSupply φ
+      (φ0:φs) = φ
   in case t0 of
-  TLam x b -> Lam (incl is fresh2) $ \x' -> eval fresh2 ((x,x'):env) b is
-  TApp f u -> app (incl is fresh2) (eval fresh2 env f is) (eval fresh1 env u)
-  TVar x -> case lookup x env of
-    Just x' -> x' is
-  TParam i t -> eval freshs env (swap t (i,fresh0)) (is++[fresh0])
-  TPair i a p -> if i `elem` is
-                 then eval fresh env p (is \\ [i])
-                 else eval fresh env a is
-  ty -> Lam (Excl is fresh) $ \v -> evalT fresh env ty is v
+  TLam x b -> Lam (incl χ φ2) $ \x' -> eval φ2 ((x,x'):ρ) b χ
+  TApp f u -> app (incl χ φ2) (eval φ2 ρ f χ) (eval φ1 ρ u)
+  TVar x -> case lookup x ρ of
+    Just x' -> x' χ
+  TParam i t -> eval φs ρ (swap t (i,φ0)) (χ++[φ0])
+  TPair i a p -> if i `elem` χ
+                 then eval φ ρ p (χ \\ [i])
+                 else eval φ ρ a χ
+  ty -> Lam (Excl χ φ) $ \v -> evalT φ ρ ty χ v
         -- NOTE: we do not put the value bound ('v') in the environment; so it's ok if it is only partial.
 
 evalT :: Colors -> Env -> Term -> Colors -> Cube Val -> Val
-evalT fresh env t0 excl v =
-  let evalB = eval fresh
-      (fresh1,fresh2) = splitSupply fresh
+evalT φ ρ t0 χ v =
+  let (φ1,φ2) = splitSupply φ
+      (φ0:φs) = φ
   in case t0 of
-  TU -> π (Excl excl fresh) v $ \_ -> Set
-  TPsi i a p -> if i `elem` excl
-                then app (Excl xi fresh) (app (incl xi fresh) (eval fresh env p xi) (face i v)) (interior i v) -- FIXME: fresh
-                else evalT fresh env a excl v -- Fixme: freshness
-    where xi = excl \\ [i]
-  TPi x a b -> π (incl excl fresh2) (eval fresh1 env a) $ \x' ->
-               evalT fresh2 ((x,x'):env) b excl (appCub v x')
-  _ -> app (Excl excl fresh) (evalB env t0 excl) v
+  TU -> π (Excl χ φ) v $ \_ -> Set
+  TPsi i a p -> if i `elem` χ
+                then app (Excl xi φ) (app (incl xi φ) (eval φ ρ p xi) (face i v)) (interior i v) -- FIXME: φ
+                else evalT φ ρ a χ v -- Fixme: freshness
+    where xi = χ \\ [i]
+  TPi x a b -> π (incl χ φ2) (eval φ1 ρ a) $ \x' ->
+               evalT φ2 ((x,x'):ρ) b χ (appCub v x')
+  TInParam i p a -> evalT φs ρ (swap p (i,φ0)) (χ++[φ0])
+                    (\xi -> if φ0 `elem` xi
+                            then v (xi \\ [φ0])
+                            else eval φ ρ a xi)
+  _ -> app (Excl χ φ) (eval φ ρ t0 χ) v
 
 -------------------------
 -- Pretty
-
-strictSublists = init . sublists
-
-sublists [] = [[]]
-sublists (x:xs) = sublists xs ++ map (x:) (sublists xs)
 
 showCols :: [Color] -> Doc
 showCols [] = "∅"
@@ -111,21 +128,9 @@ vars xs = [[x] ++ (if i > 0 then show i else "") | i <- [0..], x <- xs]
 
 freshVars = vars "xyzwstuv"
 
-False ==> _ = True
-_ ==> False = False
-
-cart :: [[a]] -> [[a]] -> [[a]]
-cart xs ys = do
-  x <- xs
-  y <- ys
-  return (x++y)
-
 showDim (Incl xs) = braces $ align $ showCols xs
 showDim (Excl is xs) = braces $ align $ (showCols is <> "/" <> showCols xs)
 
-dimLists :: Dim -> [[Color]]
-dimLists (Incl xs) = sublists xs
-dimLists (Excl is xs) = cart (strictSublists is) (sublists xs)
 
 showCube dim su v = parens $ align $ cat $ punctuate ";" $
   [showCols is <+> "↦" <+> showVal su (v is) | is <- dimLists dim ]
@@ -141,18 +146,19 @@ showVal1 su u           = parens $ showVal su u
 emptyCub = absCub "◆"
 xs `contains` ys = null (ys \\ xs)
 
+noShowNull = True
 showVal :: [String] -> Val -> Doc
 showVal _ Set           = "Set"
 showVal (s:su) (Lam dim e)
-  -- | null base =  showVal base su (e emptyCub)
+  | noShowNull, null (dimLists dim) =  showVal su (e emptyCub)
   | otherwise = "\\" <> text s <> showDim dim <+> "->" <+> showVal su (e x)
   where x = absCub s
 showVal (s:su) (Pi dim a f)
-  -- | null =  showVal su (app dim f emptyCub)
+  | noShowNull, null (dimLists dim) =  showVal su (app dim f emptyCub)
   | otherwise =  "Π(" <> text s <+> ":" <+> showCube dim su a <> ")." <+> showVal1 su (app dim f x)
   where x = absCub s
 showVal su (App dim u v)
-  -- | null =  showVal su u
+  | noShowNull, null (dimLists dim) =  showVal su u
   | otherwise = showVal su u <+> showCube dim su v
 showVal su (Var x)     = text x
 
@@ -165,53 +171,59 @@ test b fb env term = putStrLn $ show $ showCube (Incl b) freshVars $ eval fb env
 absCub :: String -> Cube Val
 absCub x js = Var $ x ++ show (showCols js)
 
-swapExTm :: Term
-swapExTm = TParam "j" (TPair "i" (TVar "a") (TVar "p") )
-
-swapExEnv = [("a",absCub "a"),("p",absCub "p"),("A",absCub "A"),("x",absCub "x"),("P", absCub "P")]
+absEnv = [("a",absCub "a"),("p",absCub "p"),("A",absCub "A"),("x",absCub "x"),("P", absCub "P")]
 
 freshBase n = take n $ map (:[]) "αβγδε"
 
-swapEx = test base fb swapExEnv swapExTm
+exSwap = test base fb absEnv $ TParam "j" (TPair "i" (TVar "a") (TVar "p") )
    where fb = freshBase 2
-         base = ["i"]
+         base = ["i","k"]
 
-exU = test boundBase fb swapExEnv TU
+exU = test boundBase fb absEnv TU
    where fb = freshBase 0
          boundBase = ["i","j"]
 
-exApp = test boundBase fb swapExEnv (TApp (TVar "P") (TVar "x"))
-   where fb = freshBase 2
+exApp = test boundBase fb absEnv (TApp (TVar "P") (TVar "x"))
+   where fb = freshBase 0
          boundBase = ["i","j"]
+exIn = test boundBase fb absEnv $ TInParam "i" (TVar "P") (TVar "x")
+   where fb = freshBase 2
+         boundBase = ["j"]
 
-exxi = test boundBase fb swapExEnv (TParam "k" $ TVar "x")
+exxi = test boundBase fb absEnv (TParam "k" $ TVar "x")
    where fb = freshBase 1
          boundBase = ["i","j"]
 
-exUi = test boundBase fb swapExEnv (TParam "k" TU)
+exUi = test boundBase fb absEnv (TParam "k" TU)
    where fb = freshBase 1
          boundBase = ["i","j"]
 
-exPredFun = test boundBase fb swapExEnv (TPi "x" (TVar "A") $ TU)
+exPredFun = test boundBase fb absEnv (TPi "x" (TVar "A") $ TU)
    where fb = freshBase 0
          boundBase = ["i","j"]
 
-exPsi = test boundBase fb swapExEnv (TPsi "i" (TVar "A") (TVar "P"))
+exPsi = test boundBase fb absEnv (TPsi "i" (TVar "A") (TVar "P"))
    where fb = freshBase 0
          boundBase = ["i","j"]
 
-exPredParam = test boundBase fb swapExEnv (TParam "k" TU `TApp` TVar "A")
-   where fb = freshBase 0
+exPsiInv = test boundBase fb absEnv $ TInParam "i" (TPsi "i" (TVar "A") (TVar "P")) (TVar "x")
+   where fb = freshBase 1
+         boundBase = ["j"]
+
+-- We should have :: (Ψi P A).i ∋ x = P x
+
+exPredParam = test boundBase fb absEnv (TParam "k" TU `TApp` TVar "A")
+   where fb = freshBase 2
          boundBase = ["i","j"]
 
-exTy = test boundBase fb swapExEnv $
+exTy = test boundBase fb absEnv $
        TParam "i" (TVar "A")
    where fb = freshBase 1
          boundBase = ["j","k"]
 
-ex = test boundBase fb swapExEnv $ (-- TParam "j" $
+exPair = test boundBase fb absEnv $ (-- TParam "j" $
                                     TPair "i" (TVar "a") (TVar "p"))
    where fb = freshBase 1
          boundBase = ["i","k"]
 
-main = ex
+main = exPair
